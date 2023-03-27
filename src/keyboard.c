7de23ede20c7142260d429e2a858de803b22a1b6
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-03-28 00:06:39 +0700

    feat: keyboard functions NOT TESTED YET, WAITING FOR INTERRUPT

--- src/keyboard.c
+++ src/keyboard.c
@@ -22,3 +22,36 @@ const char keyboard_scancode_1_to_ascii_map[256] = {
       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
 };
+
+static KeyboardDriverState keyboard_state;
+
+void keyboard_state_activate(void){
+	keyboard_state.keyboard_input_on = 1;
+}
+
+void keyboard_state_deactivate(void){
+	keyboard_state.keyboard_input_on = 0;
+}
+
+void get_keyboard_buffer(char *buf){
+	memcpy(buf, keyboard_state.keyboard_buffer, keyboard_state.keyboard_buffer_index);
+}
+
+bool is_keyboard_blocking(void){
+	return keyboard_state.keyboard_input_on;
+}
+
+void keyboard_isr(void){
+	if(!keyboard_state.keyboard_input_on){
+		keyboard_state.keyboard_buffer_index = 0;
+	} else {
+		uint8_t scancode = in(KEYBOARD_DATA_PORT);
+		char mapped_char = keyboard_scancode_1_to_ascii_map[scancode];
+		// TODO : Implement scancode processing
+		if(mapped_char != 0){
+			keyboard_state.keyboard_buffer[keyboard_state.keyboard_buffer_index] = mapped_char;
+			keyboard_state.keyboard_buffer_index++;
+		}
+	}
+	pic_ack(IRQ_KEYBOARD);
+}
