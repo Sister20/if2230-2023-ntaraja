937d2006e2392f31b285d44af549fbbd53da46ea
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-04-25 13:40:39 +0700

    chore: copy interrupt.c from guidebook

--- src/interrupt.c
+++ src/interrupt.c
@@ -4,6 +4,7 @@
 #include "lib-header/interrupt/idt.h"
 #include "lib-header/filesystem/fat32.h"
 #include "lib-header/stdmem.h"
+#include "lib-header/terminal.h"
 
 struct TSSEntry _interrupt_tss_entry = {
     .ss0 = GDT_KERNEL_DATA_SEGMENT_SELECTOR,
@@ -67,33 +68,24 @@ void set_tss_kernel_current_stack(void) {
 
 void syscall(struct CPURegister cpu, __attribute__((unused)) struct InterruptStack info) {
     if (cpu.eax == 0) {
-        struct FAT32DriverRequest request = *(struct FAT32DriverRequest *) cpu.ebx;
-        *((int8_t *) cpu.ecx) = read(request);
+        struct FAT32DriverRequest request = *(struct FAT32DriverRequest*) cpu.ebx;
+        *((int8_t*) cpu.ecx) = read(request);
     } else if (cpu.eax == 4) {
         keyboard_state_activate();
-        __asm__("sti");
+        __asm__("sti"); // Due IRQ is disabled when main_interrupt_handler() called
         while (is_keyboard_blocking());
         char buf[KEYBOARD_BUFFER_SIZE];
         get_keyboard_buffer(buf);
         memcpy((char *) cpu.ebx, buf, cpu.ecx);
     } else if (cpu.eax == 5) {
-        // TODO: Implement puts()
-//        puts((char *) cpu.ebx);
+        puts((char *) cpu.ebx, cpu.ecx, cpu.edx); // Modified puts() on kernel side
     }
 }
 
-
-void main_interrupt_handler(
-    __attribute__((unused)) struct CPURegister cpu,
-    uint32_t int_number,
-    __attribute__((unused)) struct InterruptStack info
-) {
+void main_interrupt_handler(struct CPURegister cpu, uint32_t int_number, struct InterruptStack info) {
     switch (int_number) {
-        case IRQ_KEYBOARD + PIC1:
+        case PIC1_OFFSET + IRQ_KEYBOARD:
             keyboard_isr();
-            break;
-        case PAGE_FAULT:
-            __asm__("hlt");
             break;
         case 0x30:
             syscall(cpu, info);
